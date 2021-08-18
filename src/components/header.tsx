937c42ff0311e7007d1cc1ac8a9c740075582c7f
Karolill <56150049+Karolill@users.noreply.github.com>
2021-08-18 21:52:50 +0200

    added logo in header and blobs

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import { ReactComponent as SVGLogo } from "../resources/logo.svg";
+
+const Header = () => {
+  return (
+    <header>
+      <SVGLogo />
+    </header>
+  );
+};
+
+export default Header;
