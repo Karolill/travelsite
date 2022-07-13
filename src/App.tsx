58e12b700ba5452ce465ebe2125a6e7079c58eef
Karolill <56150049+Karolill@users.noreply.github.com>
2022-07-13 15:56:30 +0200

    Added functionality to upload one image to the cloud storage

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { ReactComponent as Blob2 } from "./resources/blob2.svg";
 import { ReactComponent as Blob3 } from "./resources/blob3.svg";
 import "./styling/app.css";
 import firebase from "firebase/app";
+import "firebase/storage";
 import {
   BrowserRouter as Router,
   Switch,
@@ -19,7 +20,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAhKHqnnxevz3UjS6eS6mhrNNNynWe8O88",
   authDomain: "travelsite-4d5b3.firebaseapp.com",
   projectId: "travelsite-4d5b3",
-  storageBucket: "travelsite-4d5b3.appspot.com",
+  storageBucket: "gs://travelsite-4d5b3.appspot.com",
   messagingSenderId: "430716304889",
   appId: "1:430716304889:web:0be0147f9bb0a879be5128",
   measurementId: "G-MSCY7XKFE6",
@@ -27,6 +28,14 @@ const firebaseConfig = {
 
 // Must be called before any use of firebase
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+
+// Initialize Cloud Storage and get a reference to the service
+const storage = firebase.storage();
+
+// Create a storage reference from our storage service, more info in
+// https://firebase.google.com/docs/storage/web/create-reference#web-version-8
+const storageRef = storage.ref();
+const imagesRef = storageRef.child("images");
 
 /**
  * Component to show the application. Switch between login and
@@ -70,7 +79,7 @@ const App = () => {
             <Login />
           </Route>
           <Route path="/home">
-            <Home />
+            <Home storageRef={storageRef} />
           </Route>
         </Switch>
       </Router>
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,6 +2,11 @@ import { useContext } from "react";
 import { Redirect } from "react-router-dom";
 import { AuthContext } from "../context/UserContext";
 import HeaderSignedIn from "./headerSignedIn";
+import firebase from "firebase/app";
+
+interface HomeProps {
+  storageRef: firebase.storage.Reference;
+}
 
 /**
  * Home page. Here one will see a collection of all
@@ -9,7 +14,7 @@ import HeaderSignedIn from "./headerSignedIn";
  * new albums and edit old ones.
  * @returns A home page.
  */
-const Home = () => {
+const Home = ({ storageRef }: HomeProps) => {
   const { currentUser } = useContext(AuthContext);
 
   // If you are not loged in, redirect to the login page. Could not use
@@ -19,10 +24,31 @@ const Home = () => {
   if (!currentUser) {
     return <Redirect to={"/"} />;
   }
+
+  const clickUpload = () => {
+    console.log("Clickling");
+    // *** TRY TO SAVE AN IMAGE TO THE SERVER ***
+    // Create a reference to 'images/mountains.jpg'
+    const mountainImagesRef = storageRef.child("images/mountains.jpg");
+
+    // Must turn image into e.g. blob before saving it:
+    fetch("./resources/bilde2.jpg")
+      .then((response) => response.blob())
+      .then((blob) => {
+        // 'file' comes from the Blob or File API
+        mountainImagesRef.put(blob).then((snapshot) => {
+          console.log("Uploaded a blob or file!");
+        });
+      });
+
+    // *** DONE WITH MY ATTEMPT ***
+  };
+
   return (
     <>
       <HeaderSignedIn />
       <h1>HOME</h1>
+      <button onClick={() => clickUpload()}>Click to upload</button>
     </>
   );
 };
